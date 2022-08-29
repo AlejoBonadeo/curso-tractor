77d8e070e6c21ce646dad751ef2c8a2bd4a0abb7
Alejo Bonadeo <86572168+AlejoBonadeo@users.noreply.github.com>
2022-08-29 12:12:07 -0300

    Change Description

--- pages/temas.tsx
+++ pages/temas.tsx
@@ -2,7 +2,7 @@ import { Layout } from "../components";
 
 const Temas = () => {
   return (
-    <Layout title="Curso Tractor - Temas" description="TODO">
+    <Layout title="Curso Tractor - Temas" description="Temas a tratar en la jornada del curso">
       <div>Temas</div>
     </Layout>
   );
