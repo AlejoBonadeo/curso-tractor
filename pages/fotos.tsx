fbbb7524e2a9cb7eb8099f7c8124f1e013f0c3a5
Delfi sobrado <96445707+delfisobrado@users.noreply.github.com>
2022-08-31 09:20:16 -0300

    hoja de referencias fotos y videos

--- pages/fotos.tsx
+++ pages/fotos.tsx
@@ -1,9 +1,70 @@
-import { Layout } from "../components";
+import { Layout, CustomCard} from "../components";
+import { display } from "@mui/system";
+import { Grid, Typography, Box } from "@mui/material";
+import { Height } from "@mui/icons-material";
+
+
 
 const Fotos = () => {
     return (
         <Layout title="Curso Tractor - Fotos" description="TODO">
-        <div>Fotos</div>
+
+  <div style={{ backgroundColor: "#C5E5F5",
+    width:'100vw',
+    height: '100vh', }}>
+ <Typography variant="h3" component="h1">
+       Fotos
+      </Typography>
+      <div style={{
+    width:'100vw',
+    height: '100vh', 
+    display:'flex',
+    flexWrap: 'wrap',
+    gap: '2rem',
+    rowGap: '3rem',
+    alignContent: 'flex-start',
+    justifyContent:'space-around',
+    marginTop:'50px'}}>
+
+<div style={{backgroundColor:'white',
+width:'25%',
+height:'30%'}}> 
+
+</div>
+
+<div style={{backgroundColor:'white',
+width:'25%',
+height:'30%'}}> 
+
+</div>
+
+<div style={{backgroundColor:'white',
+width:'25%',
+height:'30%'}}> 
+
+</div>
+
+<div style={{backgroundColor:'white',
+width:'25%',
+height:'30%'}}> 
+
+</div>
+
+<div style={{backgroundColor:'white',
+width:'25%',
+height:'30%'}}> 
+
+</div>
+
+<div style={{backgroundColor:'white',
+width:'25%',
+height:'30%'}}> 
+
+</div>
+
+</div>
+
+     </div>
     </Layout>
     );
   };
--- pages/referencias.tsx
+++ pages/referencias.tsx
@@ -1,9 +1,45 @@
 import { Layout } from "../components";
+import { Box, Button, Grid, Link, Typography } from "@mui/material";
+
+const estiloReferencias ={
+backgroundColor: "#C5E5F5",
+margin: '0',
+width:'100%',
+height: '100%',
+padding: '0'
+}
 
 const Referencias = () => {
     return (
         <Layout title="Curso Tractor - Referencias" description="TODO">
-        <div>Referencias</div>
+      <div style={estiloReferencias}>
+      <Typography variant="h3" component="h1" mt={{ xs: "5vh", md: 0 }} style={{textAlign:'center'}}>
+        Referencias
+      </Typography>
+        <ul>
+          <li> </li>
+          <li> </li>
+          <li> </li>
+          <li> </li>
+          <li> </li>
+          <li> </li>
+          <li> </li>
+          <li> </li>
+          <li> </li>
+          <li> </li>
+          <li> </li>
+          <li> </li>
+          <li> </li>
+          <li> </li>
+          <li> </li>
+          <li> </li>
+          <li> </li>
+          <li> </li>
+          <li> </li>
+          <li> </li>
+          <li> </li>
+        </ul>
+         </div>
     </Layout>
     );
   };
