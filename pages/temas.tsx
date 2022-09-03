424d08b20c1d03786e9898267e4b2957352b1c7a
Alejo Bonadeo <alejobonadeo@hotmail.com>
2022-09-03 19:15:15 -0300

    Fixed Link

--- pages/temas.tsx
+++ pages/temas.tsx
@@ -1,48 +1,50 @@
 import { display } from "@mui/system";
-import { Layout, CustomCard  } from "../components";
+import { Layout, CustomCard } from "../components";
 import { ChevronRightOutlined, WhatsApp } from "@mui/icons-material";
 import { Box, Button, Grid, Link, Typography } from "@mui/material";
 import NextLink from "next/link";
 
 const Temas = () => {
   return (
-    <Layout title="Curso Tractor - Temas" description="Temas a tratar en la jornada del curso">
-<Typography variant="h3" component="h1">
-       Temas
+    <Layout
+      title="Curso Tractor - Temas"
+      description="Temas a tratar en la jornada del curso"
+    >
+      <Typography variant="h3" component="h1">
+        Temas
       </Typography>
-<Grid container mt={3} spacing={2}> 
-    <Grid item xs={12} md={4}>
-    <CustomCard
+      <Grid container mt={3} spacing={2}>
+        <Grid item xs={12} md={4}>
+          <CustomCard
             title="Tema 1"
             description={
               "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec mauris id felis commodo hendrerit. Pellentesque aliquet magna nec diam cursus consectetur. Nam tellus tortor, fermentum sit amet libero eu, fermentum volutpat nulla. Cras scelerisque varius risus quis volutpat. Praesent et vulputate libero. In vulputate augue et purus cursus ultrices. Duis malesuada risus ipsum, eget pretium mauris fermentum ut. Sed vulputate ipsum nibh, vitae cursus turpis lacinia eget."
             }
             image="https://cnnespanol.cnn.com/wp-content/uploads/2022/01/220107141734-tractor-inteligente-full-169.jpg?quality=100&strip=info"
           />
-    </Grid>
+        </Grid>
 
-    <Grid item xs={12} md={4}>
-    <CustomCard
+        <Grid item xs={12} md={4}>
+          <CustomCard
             title="Tema 2"
             description={
               "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec mauris id felis commodo hendrerit. Pellentesque aliquet magna nec diam cursus consectetur. Nam tellus tortor, fermentum sit amet libero eu, fermentum volutpat nulla. Cras scelerisque varius risus quis volutpat. Praesent et vulputate libero. In vulputate augue et purus cursus ultrices. Duis malesuada risus ipsum, eget pretium mauris fermentum ut. Sed vulputate ipsum nibh, vitae cursus turpis lacinia eget."
             }
             image="https://cnnespanol.cnn.com/wp-content/uploads/2022/01/220107141734-tractor-inteligente-full-169.jpg?quality=100&strip=info"
           />
-    </Grid>
-   
-    <Grid item xs={12} md={4}>
-    <CustomCard
+        </Grid>
+
+        <Grid item xs={12} md={4}>
+          <CustomCard
             title="Tema 3"
             description={
               "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec mauris id felis commodo hendrerit. Pellentesque aliquet magna nec diam cursus consectetur. Nam tellus tortor, fermentum sit amet libero eu, fermentum volutpat nulla. Cras scelerisque varius risus quis volutpat. Praesent et vulputate libero. In vulputate augue et purus cursus ultrices. Duis malesuada risus ipsum, eget pretium mauris fermentum ut. Sed vulputate ipsum nibh, vitae cursus turpis lacinia eget."
             }
             image="https://cnnespanol.cnn.com/wp-content/uploads/2022/01/220107141734-tractor-inteligente-full-169.jpg?quality=100&strip=info"
           />
-    </Grid>
-
-</Grid>
-<Box
+        </Grid>
+      </Grid>
+      <Box
         width="100%"
         display="flex"
         flexDirection={{ xs: "column", md: "row" }}
@@ -79,7 +81,6 @@ const Temas = () => {
         </Button>
       </Box>
     </Layout>
-
   );
 };
 
