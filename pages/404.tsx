b87d84c79b456a0206cb8868bc8a062b02ac0cd4
Alejo Bonadeo <alejobonadeo@hotmail.com>
2022-09-03 22:10:52 -0300

    Fix Error 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,29 +1,37 @@
 import NextLink from "next/link";
+import { Box, Button, Typography } from "@mui/material";
+import { ChevronRightOutlined, AgricultureOutlined } from "@mui/icons-material";
 import { Layout } from "../components";
-import { Box, Button, Grid, Link, Typography } from "@mui/material";
-import { ChevronRightOutlined} from "@mui/icons-material";
-import Home from './index';
-import { display } from '@mui/system';
-import AgricultureIcon from '@mui/icons-material/Agriculture';
 
 
 const Error404 = () => {
   return (
-    <Layout title="Error 404" description="Parece que la página que buscas no existe">
-        <div style={{backgroundColor:'#C5E5F5', 
-      display:'flex',
-      flexDirection:'column',
-      justifyContent:'center',
-      alignItems:'center',
-      height:"100vh"}}>
-
-        <h2 style={{fontSize:'70px',
-      marginTop:"0",
-      marginBottom:"10px"}}> Página  no encontrada </h2>
-<AgricultureIcon style={{fontSize:'100px',
-marginBottom:"10px"}}></AgricultureIcon>
-        <p style={{fontWeight:"bold",
-      marginBottom:"30px"}}> Parece que la página que buscas no existe </p>
+    <Layout
+      title="Error 404"
+      description="Parece que la página que buscas no existe"
+    >
+      <Box
+        bgcolor="primary.light"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        height="100vh"
+      >
+        <Typography
+          variant="h2"
+          component="h1"
+          fontWeight="bold"
+          textAlign="center"
+        >
+          Página no encontrada
+        </Typography>
+        <AgricultureOutlined
+          sx={{ fontSize: "100px", mb: 3}}
+        />
+        <Typography fontWeight="bold" mb={3}>
+          Parece que la página que buscas no existe
+        </Typography>
         <NextLink href="/index" passHref>
           <Button
             endIcon={<ChevronRightOutlined />}
@@ -32,16 +40,12 @@ marginBottom:"10px"}}></AgricultureIcon>
               fontSize: { xs: "1em", sm: "1.5em" },
               mx: { xs: 0, md: 2 },
               my: { xs: 1, md: 0 },
-              width: { xs: "100%", md: "fit-content" },
             }}
           >
-           Volver al inicio
+            Volver al inicio
           </Button>
-        </NextLink> 
-
-        </div>
-
-
+        </NextLink>
+      </Box>
     </Layout>
   );
 };
